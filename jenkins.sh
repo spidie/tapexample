f0d2147db90544e998a9b62dd4f843f58e1d3f5e
Steve Dalton <steve@refactor.com.au>
2013-08-20 21:47:34 +1000

    tap

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 bundle install
-rake db:drop RAILS_ENV=test
-rake db:create RAILS_ENV=test
-rake db:test:prepare RAILS_ENV=test
+bundle exec rake db:drop RAILS_ENV=test
+bundle exec rake db:create RAILS_ENV=test
+bundle exec rake db:test:prepare RAILS_ENV=test
+bundle exec rspec -f RSpec::TapY spec/*.rb
