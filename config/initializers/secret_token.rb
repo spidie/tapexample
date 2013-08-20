0f9918450aa0c45f799638b9ea7d3ad23644a951
Steve Dalton <steve@refactor.com.au>
2013-08-20 20:31:47 +1000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tapexample::Application.config.secret_key_base = 'd8e87dcccacd214969a0176b21533549cc9f9da85997538a2bd8f60fb4d215f52a717e1f0cafea4d5848a6b4c206961626f03f0ef87197045193d84c59ec4eb0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tapexample::Application.config.session_store :cookie_store, key: '_tapexample_session'
